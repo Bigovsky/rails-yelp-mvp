c2fad84c9fdbef5554f375ea9eadba7afea27ffc
Reda Ziatt <reda.ziatt@gmail.com>
2018-11-21 15:07:21 +0100

    reviews done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  resources :restaurants, except: [:destroy, :update, :edit]
+  get 'reviews/new'
+  get 'reviews/create'
+  resources :restaurants, except: [:destroy, :update, :edit] do
+   resources :reviews, only: [:new, :create]
+  end
 end
